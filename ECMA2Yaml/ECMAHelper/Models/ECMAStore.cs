8f7b424ee13f0806cb0ec669773fd047ad926fa1
Tianqi Zhang <tianzh@microsoft.com>
2019-05-22 12:26:56 +0800

    detect self-inheriting classes

--- ECMA2Yaml/ECMAHelper/Models/ECMAStore.cs
+++ ECMA2Yaml/ECMAHelper/Models/ECMAStore.cs
@@ -700,6 +700,10 @@ namespace ECMA2Yaml.Models
             {
                 t.InheritanceUids = new List<string>();
                 string baseUid = t.BaseType.Uid;
+                if (baseUid == t.Uid)
+                {
+                    return;
+                }
                 AddInheritanceMapping(t.Uid, baseUid);
                 do
                 {
